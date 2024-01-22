9deb35f82733190b1af1171e0af3e3020a8b3f4c
noliuniverse <92001678+noliuniverse@users.noreply.github.com>
2024-01-22 12:19:07 -0500

    Update homepage message

--- app/page.js
+++ app/page.js
@@ -60,7 +60,8 @@ if (loading) {return (
       <div className='div1'>
       <h1 className='whitetext bold big' style={{color: "rgb(127, 86, 201)"}}>Welcome to UMBRA!</h1>
         <p className='whitetext'>UMBRA is a fan-made cosmo client where people can collect custom objekts made by other fans. Ways of getting them include cupsleeve events, tripleS fan meetups, and etc! Sign up using the login button above!</p>
-
+        <br></br>
+        <p className='whitetext'>If you want to include an objekt of yours in UMBRA, contact @wavnoil on twitter.</p>
         </div>
       </main>
       )}
