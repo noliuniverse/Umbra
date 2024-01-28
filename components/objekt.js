3ac148d4ee6bcfb4ab6afb2e142800e2d8af166c
noliuniverse <92001678+noliuniverse@users.noreply.github.com>
2024-01-28 16:15:57 -0500

    Fix NEW Label

--- components/objekt.js
+++ components/objekt.js
@@ -62,7 +62,7 @@ const Objekt = ( { unique, bckcolor, color, created_at, id, serial, img, uuid, m
                     {serial && <span style={dotMat.style} className="objekt_preview_text3">#{serial.toString().padStart(5, '0')}</span>}
                 </div>
                 <div style={helveticaNeueBold.style}>
-                {created_at && <div style={{ position: "absolute", bottom: dimensions.width/7.5/4.5, left: dimensions.width/7.5/5,background: "rgb(127, 86, 201)", paddingRight: "5%", paddingLeft: "5%",borderRadius: "10px", height:  dimensions.width/7.5}} className="whitetext"><span style={{margin: "auto", fontSize: "70%"}}>NEW</span></div>}
+                {created_at && <div style={{ position: "absolute", bottom: dimensions.width/7.5/4.5, left: dimensions.width/7.5/5,background: "rgb(127, 86, 201)", paddingRight: "5%", paddingLeft: "5%",borderRadius: "10px", textAlign: "center",margin: "auto", fontSize: "70%"}} className="whitetext">NEW</div>}
                 </div>
                 </div>
             <div style={{background: bckcolor,width:"100%", height:"0px", margin: "auto",borderRadius: "10px", overflow: "hidden"}} id={unique}>
