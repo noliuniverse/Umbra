80a7a8e4bb737a1b0faec112aa86b86ea2cf7a1f
noliuniverse <geniusmusically@gmail.com>
2024-04-23 12:09:20 -0400

    Fix double press position

--- app/collection/page.js
+++ app/collection/page.js
@@ -208,7 +208,7 @@ if(!mounted) return null;
             </header>
             <div className="div1" style={{paddingBottom: "10px"}}>
                 <p className="whitetext"><small>{translate('username')}: </small><span className="big bold">{user_name}</span></p>
-                <small className="whitetext" style={{ fontSize:"80%", position:"absolute", left:"10%", right:"10%", top:"21.2%"}}>{translate('doublepress')}</small>
+                <small className="whitetext" style={{ fontSize:"80%", position:"absolute", left:"10%", right:"10%", top:"173px"}}>{translate('doublepress')}</small>
                 <br/>
                 <Suspense>
             {datas && <div style={{paddingBottom: "20px", paddingTop: "10px"}}> <FetchMoreObjekts datas={datas} userid={user.id}></FetchMoreObjekts></div>}
